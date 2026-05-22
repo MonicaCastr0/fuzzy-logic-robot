8522e17ae0ccf9bb3f9bcd829b3a74b926dc0d89
na1tto <eduardo3245.ss@gmail.com>
2026-05-22 10:38:51 -0300

    feat: implemented TB6612FNG motor driver

--- components/motor_driver/include/MotorDriver.hpp
+++ components/motor_driver/include/MotorDriver.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
 class MotorDriver {
 public:
+    //simple public interface to control the motors without exposing GPIO and PWM details
     MotorDriver();
 
     void init();
@@ -10,4 +14,23 @@ public:
 
     void drive(int motorASpeed, int motorBSpeed);
     void stop();
+
+private:
+    struct MotorPins {
+        gpio_num_t in1;
+        gpio_num_t in2;
+        gpio_num_t pwm;
+        ledc_channel_t channel;
+    };
+
+    //private methods to hide GPIO and PWM details
+    void setupGpio();
+    void setupPwm();
+
+    void setupPwmChannel(gpio_num_t pwmPin, ledc_channel_t channel);
+    void setMotor(const MotorPins& motor, int speed);
+    void setPwmDuty(ledc_channel_t channel, int duty);
+
+    int clampSpeed(int speed) const;
+    int absoluteValue(int value) const;
 };
